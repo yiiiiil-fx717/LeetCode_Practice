db0436bbe10467ecf4295c8c21f92e861817e913
yliu <yliu308@hawk.iit.edu>
2018-05-12 00:54:53 -0500

    Fourth Update

---
+++ Leetcode_226.java
@@ -0,0 +1,31 @@
+package Leetcode;
+
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Leetcode_226 {
+	public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode current = queue.poll();
+            TreeNode swap = current.right;
+            current.right = current.left;
+            current.left = swap;
+            
+            if(current.left != null) queue.add(current.left);
+            if(current.right != null) queue.add(current.right);
+        }
+        return root;
+    }
+}
---
+++ Leetcode_237.java
@@ -0,0 +1,16 @@
+package Leetcode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Leetcode_237 {
+	public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
