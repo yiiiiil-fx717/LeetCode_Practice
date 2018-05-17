dab7f9b260423f9ec0196cdfa551e4c9e2785c1f
yliu <yliu308@hawk.iit.edu>
2018-05-17 11:55:44 -0500

    Update 100th

---
+++ Leetcode_100.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Leetcode_100 {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }else if(p == null || q == null){
+            return false;
+        }else{
+            return (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        }
+    }
+}
