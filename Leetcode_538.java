4c99b64d976a68db03d7c5e9afa561bfef00ef59
yliu <yliu308@hawk.iit.edu>
2018-05-17 13:09:17 -0500

    Update 538th

---
+++ Leetcode_538.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Leetcode_538 {
+	private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
