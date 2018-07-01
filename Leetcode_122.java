1ed3d0ac492686db98adf2faad5ee5b96cfca409
yliu <yliu308@hawk.iit.edu>
2018-07-01 18:11:41 -0500

    2018/07/01

---
+++ Leetcode_122.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+public class Leetcode_122 {
+	public int maxProfit(int[] prices) {
+        int result = 0;
+        
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i - 1]){
+                result += prices[i] - prices[i - 1];
+            }
+        }
+        return result;
+    }
+}
